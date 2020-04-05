c2589116231cbab4973c20b2884a9d8f7e2c8072
mike <michael.lloyd03@gmail.com>
2020-04-05 00:23:36 -0700

    select and remove of larger area elems works now

--- interfaceFunctions/selection.js
+++ interfaceFunctions/selection.js
@@ -1,17 +1,16 @@
 import {shiftKey} from '../index.js'
 
 export const selectElement = (event) => {
-  console.log(event.target)
   if (event.target.tagName == 'svg' && $('.selected').length) {
     $('.selected').removeClass('selected')
   }
   else if (event.target.tagName == 'svg' && !$('.selected').length) {
   }
   else if (shiftKey) {
-    $(`#${event.target.id}`).addClass('selected')
+    $(`#${event.target.parentNode.id}`).addClass('selected')
   }
   else {
     $('.selected').removeClass('selected')
-    $(`#${event.target.id}`).addClass('selected')
+    $(`#${event.target.parentNode.id}`).addClass('selected')
   }
 }
